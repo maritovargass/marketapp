affa1a939b70b9efa17164559984557c20623573
Mario <mvargas@qantamedia.com>
2014-11-18 19:40:01 -0600

    Add order history

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,13 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   respond_to :html
+def sales
+  @orders = Order.all.where(seller: current_user).order("created_at DESC")
+end
 
-  def index
-    @orders = Order.all
-    respond_with(@orders)
-  end
-
-  def show
-    respond_with(@order)
-  end
+def purchases
+  @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+end
 
   def new
     @order = Order.new
@@ -19,8 +17,6 @@ class OrdersController < ApplicationController
     respond_with(@order)
   end
 
-  def edit
-  end
 
   def create
     @order = Order.new(order_params)
@@ -33,16 +29,6 @@ class OrdersController < ApplicationController
     @order.save
     flash[:notice] = 'Order was successfully created.' if @order.save
 respond_with(@order, :location => root_url)
-  end
-
-  def update
-    @order.update(order_params)
-    respond_with(@order)
-  end
-
-  def destroy
-    @order.destroy
-    respond_with(@order)
   end
 
   private
